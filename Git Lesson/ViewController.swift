036c7a490845c81c95efee88f38cc45d94a08599
Jordan Lamb <jdawg2329@gmail.com>
2019-09-20 14:58:14 -0600

    first commit

---
+++ Git Lesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Lesson
+//
+//  Created by Jordan Lamb on 9/20/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
